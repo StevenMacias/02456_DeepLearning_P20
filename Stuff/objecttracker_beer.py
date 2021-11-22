41b6f27139c8b6389cc8a7bba95f01c4ae9f2798
Hofferlaffen <93922865+Hofferlaffen@users.noreply.github.com>
2021-11-22 17:02:43 +0100

    Update objecttracker_beer.py
    updated objecttracker to work with torch model

--- Stuff/objecttracker_beer.py
+++ Stuff/objecttracker_beer.py
@@ -8,6 +8,7 @@ import time
 import cv2
 import os
 import torch
+from torchvision import transforms
 path = os.path.dirname(os.path.abspath(__file__))
 import sys
 sys.path.append(path)
@@ -28,6 +29,8 @@ print("[INFO] loading model...")
 net = torch.load(args["model"])
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 net.to(device)
+convert_tensor = transforms.toTensor()
+net.eval()
 # initialize the video stream and allow the camera sensor to warmup
 print("[INFO] starting video stream...")
 vs = VideoStream(src=0).start()
@@ -36,50 +39,50 @@ time.sleep(2.0)
 # loop over the frames from the video stream
 while True:
 	# read the next frame from the video stream and resize it
-	frame = vs.read()
-	frame = imutils.resize(frame, width=400)
+    frame = vs.read()
+    frame = imutils.resize(frame, width=6)
+    frame = convert_tensor(frame)
 	# if the frame dimensions are None, grab them
-	if W is None or H is None:
-		(H, W) = frame.shape[:2]
-
+    if W is None or H is None:
+        (H, W) = frame.shape[:2]
 	# obtain our output predictions, and initialize the list of
 	# bounding box rectangles
-	net.setInput(frame)
-	detections = net.forward()
-	rects = []
+    with torch.no_grad():
+        detections = net([frame.to(device)])
+        rects = []
     
     # loop over the detections
-	for i in range(0, detections.shape[2]):
+    for i in range(0, detections.shape[2]):
 		# filter out weak detections by ensuring the predicted
 		# probability is greater than a minimum threshold
-		if detections[0, 0, i, 2] > args["confidence"]:
+        if detections[0, 0, i, 2] > args["confidence"]:
 			# compute the (x, y)-coordinates of the bounding box for
 			# the object, then update the bounding box rectangles list
-			box = detections[0, 0, i, 3:7] * np.array([W, H, W, H])
-			rects.append(box.astype("int"))
+            box = detections[0, 0, i, 3:7] * np.array([W, H, W, H])
+            rects.append(box.astype("int"))
 			# draw a bounding box surrounding the object so we can
 			# visualize it
-			(startX, startY, endX, endY) = box.astype("int")
-			cv2.rectangle(frame, (startX, startY), (endX, endY),
+            (startX, startY, endX, endY) = box.astype("int")
+            cv2.rectangle(frame, (startX, startY), (endX, endY),
 				(0, 255, 0), 2)
             
     # update our centroid tracker using the computed set of bounding
 	# box rectangles
-	objects = ct.update(rects)
+    objects = ct.update(rects)
 	# loop over the tracked objects
-	for (objectID, centroid) in objects.items():
+    for (objectID, centroid) in objects.items():
 		# draw both the ID of the object and the centroid of the
 		# object on the output frame
-		text = "ID {}".format(objectID)
-		cv2.putText(frame, text, (centroid[0] - 10, centroid[1] - 10),
-			cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-		cv2.circle(frame, (centroid[0], centroid[1]), 4, (0, 255, 0), -1)
+        text = "ID {}".format(objectID)
+        cv2.putText(frame, text, (centroid[0] - 10, centroid[1] - 10),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+        cv2.circle(frame, (centroid[0], centroid[1]), 4, (0, 255, 0), -1)
 	# show the output frame
-	cv2.imshow("Frame", frame)
-	key = cv2.waitKey(1) & 0xFF
+    cv2.imshow("Frame", frame)
+    key = cv2.waitKey(1) & 0xFF
 	# if the `q` key was pressed, break from the loop
-	if key == ord("q"):
-		break
+    if key == ord("q"):
+        break
 # do a bit of cleanup
 cv2.destroyAllWindows()
 vs.stop()
