1eabfa84cdf79e87fcd1ef3b7f5d93f2d5d16e1f
Hofferlaffen <93922865+Hofferlaffen@users.noreply.github.com>
2021-11-23 10:57:36 +0100

    typo corrected

--- Stuff/objecttracker_beer.py
+++ Stuff/objecttracker_beer.py
@@ -29,7 +29,7 @@ print("[INFO] loading model...")
 net = torch.load(args["model"])
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 net.to(device)
-convert_tensor = transforms.toTensor()
+convert_tensor = transforms.ToTensor()
 net.eval()
 # initialize the video stream and allow the camera sensor to warmup
 print("[INFO] starting video stream...")
